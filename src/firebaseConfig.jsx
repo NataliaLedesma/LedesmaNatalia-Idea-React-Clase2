4a6559d60e44f367037ed09b9a7173bc80c7e88d
NataliaLedesma <natalia.ledesma92@gmail.com>
2022-10-11 23:09:44 -0300

    m

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQi3p2XWR9Hw-ETSutBuFi_igkDjiWJaU",
-    authDomain: "tienda-tiggy.firebaseapp.com",
-    projectId: "tienda-tiggy",
-    storageBucket: "tienda-tiggy.appspot.com",
-    messagingSenderId: "481918353516",
-    appId: "1:481918353516:web:f07f435ef1b1ef662d5849"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
 };
 
 
